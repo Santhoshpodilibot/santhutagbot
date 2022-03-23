c7771656a658415bf2abb8db2764767f5cdf8ccf
Santhosh podili <99010597+Santhupodili@users.noreply.github.com>
2022-03-23 16:49:50 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,5 +1,5 @@
 import os, logging, asyncio
-from telethon.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from telethon.types import Button
 from telethon import TelegramClient, events
 from telethon.tl.types import ChannelParticipantAdmin
 from telethon.tl.types import ChannelParticipantCreator
